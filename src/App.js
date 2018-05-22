9073bf2b397c21c9a9fd0639d351d5c2ffdd014d
Daniel Andrews <daniel@revelry.co>
2018-05-21 21:47:19 -0500

    Refactor form to use new context API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React, { Component } from 'react';
+const FamilyContext = React.createContext();
 
-const Person = props => (
-  <li className="Person">
-    <div className="Person-name">{props.person.name}</div>
-    <div className="Person-memberType">{props.person.member}</div>
-  </li>
-);
-
-const Family = props => (
-  <ul className="family">
-    {props.members.map(member =>
-      <Person key={member.name} person={member} />
-    )}
-  </ul>
-);
-
-class App extends Component {
+class FamilyProvider extends Component {
   state = {
-    family: [{ name: 'Daniel', member: 'Me' }],
-    results: {}
+    results: {},
+    family: [{name: 'Daniel', member: 'Me'}]
   };
 
   onInputChange = (e) => {
@@ -31,45 +17,93 @@ class App extends Component {
 
   addFamilyMember = e => {
     e.preventDefault();
+
     const { results } = this.state;
 
     if (!results.name || !results.member) return;
 
     const family = this.state.family.concat([{
-      name: this.state.results.name,
-      member: this.state.results.member
+      name: results.name,
+      member: results.member
     }]);
 
     this.setState({ family });
   };
 
-  render() {
-    const { family } = this.state;
+  get actions() {
+    return {
+      addFamilyMember: this.addFamilyMember,
+      onInputChange: this.onInputChange
+    };
+  };
 
+  render() {
     return (
-      <div className="App">
+      <FamilyContext.Provider
+        value={{
+          state: this.state,
+          actions: this.actions
+        }}
+      >
+        {this.props.children}
+      </FamilyContext.Provider>
+    );
+  }
+}
+
+const Person = props => (
+  <div className="Person">
+    <div className="Person-name">{props.person.name}</div>
+    <div className="Person-member">{props.person.member}</div>
+  </div>
+);
+
+const Family = props => (
+  <FamilyContext.Consumer>
+    {context => (
+      <React.Fragment>
+        {context.state.family.map(person =>
+          <Person key={person.name} person={person} />
+        )}
+      </React.Fragment>
+    )}
+  </FamilyContext.Consumer>
+);
+
+const FamilyMemberForm = props => (
+  <FamilyContext.Consumer>
+    {context => (
+      <form onSubmit={context.actions.addFamilyMember}>
+        <div className="FormRow">
+          <label htmlFor="name">Name: </label>
+          <input
+            name="name"
+            onChange={context.actions.onInputChange}
+          />
+        </div>
+        <div className="FormRow">
+          <label htmlFor="member">Family Member: </label>
+          <input
+            name="member"
+            onChange={context.actions.onInputChange}
+          />
+        </div>
+        <div className="FormRow">
+          <button>Add a Family Member</button>
+        </div>
+      </form>
+    )}
+  </FamilyContext.Consumer>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <FamilyProvider>
         <h1>{"My Family's Context"}</h1>
-        <Family members={family} />
-        <form onSubmit={this.addFamilyMember}>
-          <div className="FormRow">
-            <label htmlFor="name">Name: </label>
-            <input
-              name="name"
-              onChange={this.onInputChange}
-            />
-          </div>
-          <div className="FormRow">
-            <label htmlFor="member">Family Member: </label>
-            <input
-              name="member"
-              onChange={this.onInputChange}
-            />
-          </div>
-          <div className="FormRow">
-            <button>Add a Family Member</button>
-          </div>
-        </form>
-      </div>
+        <Family />
+        <FamilyMemberForm />
+      </FamilyProvider>
     );
   }
 }
